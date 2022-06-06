3fe342e978b565c31de984c282b816c3528fbc7b
NazarK0 <staticman999@gmail.com>
2022-06-06 21:49:04 +0300

    structs

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,55 @@
+struct User {
+  active: bool,
+  username: String,
+  email: String,
+  sign_in_count: u64,
+}
+
+struct Color(i8, i8, i8);
+
+struct AlwaysEqual;
+
+
 fn main() {
-  let s1 = String::from("hello");
-  
-  // let s2 = s1;
-  
-  let s2 = s1.clone();
-  
-  println!("s1 = {}, s2 = {}", s1, s2);
+  let user1 = User {
+    email: String::from("test@example.com"),
+    username: String::from("Jon Doe"),
+    active: true,
+    sign_in_count: 1,
+  };
 
-  let s = String::from("My first string in rust");
-  let w1 = first_word(&s);
-  // s.clear();
-  println!("word #1 = {}", w1);
+  let mut user2 = User {
+    email: String::from("alucard@example.com"),
+    username: String::from("Dracula"),
+    active: true,
+    sign_in_count: 1,
+  };
 
-  // array slice 
-  let a = [1, 2, 3, 4, 5, 6, 7, 8];
-  let slice = &a[1..3];
-  assert_eq!(slice, &[2,3]);
+  user2.email = String::from("changed@gmail.com");
 
+  // println!("user1: {:#?}", user1);
 
-  
+  // struct update syntax
+  // we can no longer use user1 after creating user3
+  let user3 = User {
+    email: String::from("another@example.com"),
+    ...user1
+  };
+
+  // tuple struct
+  let black = Color(0, 0, 0);
+
+  // unit struct
+  let subject = AlwaysEqual;
+
 }
 
-// find out first word in line ard return 
-// string slice
-fn first_word(s: &str) -> &str {
-  let bytes = s.as_bytes();
-
-  for (i, &item) in bytes.iter().enumerate() {
-    if item == b' ' {
-      return &s[..i];
-    }
+fn build_user(email: String, username: String) -> User {
+  User {
+    email,
+    username,
+    active: true,
+    sign_in_count: 1,
   }
-
-  &s[..]
 }
+
