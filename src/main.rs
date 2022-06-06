79aaed41ea314d0dcb5ee12b545b478e27d64ba7
NazarK0 <staticman999@gmail.com>
2022-06-06 21:09:56 +0300

    slices

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
 fn main() {
-  println!("Hello world");
+  let s1 = String::from("hello");
+  
+  // let s2 = s1;
+  
+  let s2 = s1.clone();
+  
+  println!("s1 = {}, s2 = {}", s1, s2);
 
-  another_function(8, 'm');
+  let s = String::from("My first string in rust");
+  let w1 = first_word(&s);
+  // s.clear();
+  println!("word #1 = {}", w1);
+
+  // array slice 
+  let a = [1, 2, 3, 4, 5, 6, 7, 8];
+  let slice = &a[1..3];
+  assert_eq!(slice, &[2,3]);
+
+
+  
 }
 
-fn another_function(x: i8, label: char) {
-  println!("x = {}({})", x, label);
+// find out first word in line ard return 
+// string slice
+fn first_word(s: &str) -> &str {
+  let bytes = s.as_bytes();
+
+  for (i, &item) in bytes.iter().enumerate() {
+    if item == b' ' {
+      return &s[..i];
+    }
+  }
+
+  &s[..]
 }
