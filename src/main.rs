3c722a489b9f54a7bafaf9272200160f020cabd6
NazarK0 <staticman999@gmail.com>
2022-06-07 21:34:05 +0300

    structs, display struct

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,24 @@
-struct User {
-  active: bool,
-  username: String,
-  email: String,
-  sign_in_count: u64,
+#[derive(Debug)]
+struct Rectangle {
+  width: u32, 
+  height: u32,
 }
-
-struct Color(i8, i8, i8);
-
-struct AlwaysEqual;
-
 
 fn main() {
-  let user1 = User {
-    email: String::from("test@example.com"),
-    username: String::from("Jon Doe"),
-    active: true,
-    sign_in_count: 1,
+  let rect1 = Rectangle {
+    width: 30,
+    height: 50
   };
 
-  let mut user2 = User {
-    email: String::from("alucard@example.com"),
-    username: String::from("Dracula"),
-    active: true,
-    sign_in_count: 1,
-  };
+  println!("The area of the rectongle is {} square pixels.",
+    area(&rect1));
 
-  user2.email = String::from("changed@gmail.com");
+  println!("rect1 is {:#?}", rect1);
 
-  // println!("user1: {:#?}", user1);
-
-  // struct update syntax
-  // we can no longer use user1 after creating user3
-  let user3 = User {
-    email: String::from("another@example.com"),
-    ...user1
-  };
-
-  // tuple struct
-  let black = Color(0, 0, 0);
-
-  // unit struct
-  let subject = AlwaysEqual;
+  dbg!(&rect1);
 
 }
 
-fn build_user(email: String, username: String) -> User {
-  User {
-    email,
-    username,
-    active: true,
-    sign_in_count: 1,
-  }
+fn area(rectangle: &Rectangle) -> u32 {
+  rectangle.width * rectangle.height
 }
-
