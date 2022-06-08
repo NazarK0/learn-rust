3a6b7ea02ac68896305b3e3a74426948ed573a85
NazarK0 <staticman999@gmail.com>
2022-06-08 17:48:52 +0300

    'privacy'

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,61 @@
+mod front_of_house {
+   pub mod hosting {
+       pub fn add_to_waitlist() {}
+       fn seat_at_table() {}
+   }
+
+   mod serving {
+       fn take_order() {}
+       fn serve_order() {}
+       fn take_payment() {}
+   }
+}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+    // absolute path
+    // crate::front_of_house::hosting::add_to_waitlist();
+
+    // relative path
+    // front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // meal.seasonal_fruit = String::from("bluberies");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+
+}
+
+fn deliver_order() {}
---
+++ restaurant/target/debug/librestaurant.d
@@ -0,0 +1,1 @@
+D:\programming\projects\learn-rust\restaurant\target\debug\librestaurant.rlib: D:\programming\projects\learn-rust\restaurant\src\lib.rs
