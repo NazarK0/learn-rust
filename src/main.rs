7b1b9eb9320f5b77d473870c42d4b2d88809c9ba
NazarK0 <staticman999@gmail.com>
2022-06-08 07:11:10 +0300

    Option<T> enum, if let, match

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,21 @@ impl Message {
   }
 }
 
+enum Coin {
+  Penny,
+  Nickel,
+  Dime,
+  Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+  Alabama,
+  Alaska,
+  Iowa,
+  Texas
+}
+
 struct IpAddr {
   kind: IpAddrKind,
   address: String,
@@ -36,9 +51,9 @@ fn main() {
   };
 
   let loopback = IpAddr {
-    kind: IpAddrKind:v6,
-    addess: String::from(":::1")
-  }
+    kind: IpAddrKind::v6,
+    address: String::from(":::1")
+  };
 
   let home2 = IpAddress::v4(127, 0, 0, 1);
   let loopback2 = IpAddress::v6(String::from(":::1"));
@@ -52,8 +67,51 @@ fn main() {
 
   let absent_mumber: Option<i32> = None;
 
+  // let x: i8 = 6;
+  // let y: Option<i8> = Some(6);
+  // let sum = x + y;
+
+  let two = Some(2);
+  let three = plus_one(two);
+  let none = plus_one(None);
+
+  let config_max = Some(3u8);
+
+  // using match
+  match config_max {
+    Some(max) => println!("The maximum is cofigured to be {}", max),
+    _ => (),
+  }
+
+  // using if let
+  if let Some(max) = config_max {
+    println!("The maximum is configured to be {}", max);
+  }
+
 }
 
 fn route(ip_kind: IpAddrKind) {
 
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+  match coin {
+    Coin::Penny => {
+      println!("Lucky penny!");
+      1
+    },
+    Coin::Nickel => 5,
+    Coin::Dime => 10,
+    Coin::Quarter(state) => {
+      println!("State quarter from {:?}!", state);
+      25
+    },
+  }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+  match x {
+    None => None,
+    Some(i) => Some(i + 1),
+  }
 }
