0a1d1adf119aacc40ce3f723b7849bca412fa275
NazarK0 <staticman999@gmail.com>
2022-06-09 10:51:38 +0300

    propagating error

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,16 +1,23 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::{self, Read};
 
 fn main() {
-    let f = File::open("hello.txt")
-      .unwrap_or_else(|error| {
-          if error.kind() == ErrorKind::NotFound {
-              File::create("hello.txt")
-                .unwrap_or_else(|error| {
-                    panic!("Promlem crating the file: {:?}", error);
-                }) 
-          } else {
-              panic!("Problem opening the file: {:?}", error);
-          }
-      });
+    let username = read_username_from_file().unwrap();
+    println!("username: {}",username);
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
