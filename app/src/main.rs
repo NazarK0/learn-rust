f6cee596a4f97042098f5fc3a0c0f70ed60fa78c
NazarK0 <staticman999@gmail.com>
2022-06-09 15:00:50 +0300

    propagating error, very short. Return Result from main()

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,15 +1,12 @@
-use std::fs::File;
-use std::io::{self, Read};
+use std::{fs, io, error};
 
-fn main() {
+fn main() -> Result<(), Box<dyn error::Error>> {
     let username = read_username_from_file().unwrap();
     println!("username: {}",username);
+
+    Ok(())
 }
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let mut s = String::new();
-
-    File::open("hello.txt")?.read_to_string(&mut s)?;
-    
-    Ok(s)
+    fs::read_to_string("hello.txt")
 }
