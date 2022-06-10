69dea226fef72a2daf4e6019eb8ecf00cbaeddaa
NazarK0 <staticman999@gmail.com>
2022-06-10 09:56:32 +0300

    'generics'

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,12 +1,51 @@
-use std::{fs, io, error};
+use std::fmt::{Display, Debug};
 
-fn main() -> Result<(), Box<dyn error::Error>> {
-    let username = read_username_from_file().unwrap();
-    println!("username: {}",username);
-
-    Ok(())
+struct Pair<T> {
+    x: T,
+    y: T,
 }
 
-fn read_username_from_file() -> Result<String, io::Error> {
-    fs::read_to_string("hello.txt")
+impl<T> Pair<T> {
+    fn new(x:T, y: T) -> Self {
+        Self{ x, y }
+    }
 }
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result)
+}
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn some_function<T, U>(t: &T, u: &U) -> i32
+  where T: Display + Clone,
+        U: Clone + Debug
+{
+    4
+}
+
