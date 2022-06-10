0a3a025d0ccc6fb328cec89a7210f78af0189c07
NazarK0 <staticman999@gmail.com>
2022-06-10 13:46:50 +0300

    'lifetimes'

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,51 +1,56 @@
-use std::fmt::{Display, Debug};
+use std::fmt::Display;
 
-struct Pair<T> {
-    x: T,
-    y: T,
-}
-
-impl<T> Pair<T> {
-    fn new(x:T, y: T) -> Self {
-        Self{ x, y }
-    }
-}
-
-impl<T: Display + PartialOrd> Pair<T> {
-    fn cmp_display(&self) {
-        if self.x >= self.y {
-            println!("The largest member is x = {}", self.x);
-        } else {
-            println!("The largest member is y = {}", self.y);
-        }
-    }
+// Lifetime Annotations in Struct Definitions
+struct ImportantExcerpt<'a> {
+    part: &'a str,
 }
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
-    println!("The largest number is {}", result)
+    // The Static Lifetime
+    let s: &'static str = "I have a static lifetime.";
+
+    let mut result;
+    result = longest(string1.as_str(), string2);
+
+    println!("[1] the longest string is: {}", result);
+    
+    // error code, not compile
+    // {
+    //     let string3 = String::from("long string is long");
+    //     result = longest(string1.as_str(), string3.as_str());
+    // }
+    // println!("[2] the longest string is: {}", result);
+
+    let novel = String::from("Call me Ishael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
 
-fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
-    let mut largest = list[0];
-
-    for &item in list {
-        if item > largest {
-            largest = item;
-        }
+// lifetime annotation 'a
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
-
-    largest
 }
 
-fn some_function<T, U>(t: &T, u: &U) -> i32
-  where T: Display + Clone,
-        U: Clone + Debug
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str, 
+    y: &'a str, 
+    ann: T,
+) -> &'a str 
+where
+    T:Display,
 {
-    4
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
 }
-
