841a0c2b51138f3305e446fb43888a394dd8b8c9
NazarK0 <staticman999@gmail.com>
2022-06-14 11:37:44 +0300

    closures with generics

--- iterators_and_closures/src/main.rs
+++ iterators_and_closures/src/main.rs
@@ -1,38 +1,37 @@
-use std::{thread, time::Duration, collections::HashMap};
+use std::{thread, time::Duration, collections::HashMap, hash, fmt};
 
-#[derive(Debug, Hash, Eq)]
-struct Cacher<A,T,F>
+struct Cacher<A,R,F>
 where
-    F: Fn(A) -> T,
+    F: Fn(A) -> R,
+    A: Eq + PartialEq + hash::Hash + Copy,
+    R: Copy,
 {
     calculation: F,
-    cache: HashMap<A, T>,
-    value: Option<T>,
+    cache: HashMap<A, R>,
 }
 
-impl<A,T,F> Cacher<A,T,F>
+impl<A,R,F> Cacher<A,R,F>
 where
-    F: Fn(A) -> T,
+    F: Fn(A) -> R,
+    A: Eq + PartialEq + hash::Hash + Copy + fmt::Display,
+    R: Copy+fmt::Display,
 {
-    fn new(calculation: F) -> Cacher<A,T,F> {
+    fn new(calculation: F) -> Cacher<A,R,F> {
         Cacher {
             calculation,
             cache: HashMap::new(),
-            value: None,
         }
     }
 
-    fn value(&mut self, arg: A) -> T {
-
-        self.cache.entry(arg).or_insert((self.calculation)(arg));
-        // match self.cache.get(arg) {
-        //     Some(v) => v,
-        //     None => {
-        //         let v = (self.calculation)(arg);
-        //         self.value = Some(v);
-        //         v
-        //     }
-        // }
+    fn value(&mut self, arg: A) -> R {
+        match self.cache.get(&arg) {
+            Some(v) => *v,
+            None => {
+                let v = (self.calculation)(arg);
+                self.cache.insert(arg, v);
+                v
+            }
+        }
     }
 }
 
@@ -41,12 +40,6 @@ fn main() {
     let simulated_random_number = 3;
 
     generate_workout(simulated_user_specified_value, simulated_random_number);
-}
-
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculating slowly...");
-    thread::sleep(Duration::from_secs(3));
-    intensity
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
@@ -59,20 +52,26 @@ fn generate_workout(intensity: u32, random_number: u32) {
     if intensity < 25 {
         println!(
             "Today do {} pushups!",
-            expensive_result.value(intensity)
+            expensive_result.value(intensity-2)
         );
 
         println!(
             "Next, do {} situps!",
-            expensive_result.value(intensity)
+            expensive_result.value(intensity-5)
+        );
+
+        println!(
+            "Next, do {} pullups!",
+            expensive_result.value(intensity-5)
         );
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
+            let run_intensity = ((intensity as f64) * 1.12).round() as u32;
             println!(
                 "Today, run for {} minutes!",
-                expensive_result.value(intensity)
+                expensive_result.value(run_intensity)
             );
         }
     }
