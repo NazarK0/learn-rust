d3fb4357cfddfcbed40fd7b14c9f043d8972b8ec
NazarK0 <staticman999@gmail.com>
2022-06-14 13:19:18 +0300

    impl iterator

--- iterators_and_closures/src/main.rs
+++ iterators_and_closures/src/main.rs
@@ -1,8 +1,43 @@
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0}
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
 fn main() {
     let x = vec![1, 2, 3];
-    let equal_to_x = move |z| z == x;
-    // println!("can`t use x here: {:?}", x);
+    let x1_iter = x.iter();
+    
+    // for val in x1_iter {
+    //     println!("Got: {}", val);
+    // }
 
-    let y = vec![1, 2, 3];
-    assert!(equal_to_x(y));
+    // let total: i32 = x1_iter.sum();
+
+    // do nothing, iterators are lazy
+    // x1_iter.map(|x| x + 1);
+
+
+    let x2: Vec<_> =  x1_iter.map(|x| x + 1).collect();
+
+    for elem in Counter::new() {
+        println!("Count: {}", elem);
+    }
 }
