3f829985a263a6a4b695427b5c17ab389a4b4cc9
NazarK0 <staticman999@gmail.com>
2022-06-18 11:21:49 +0300

    Rust pattern

--- blog/src/main.rs
+++ blog/src/main.rs
@@ -2,19 +2,14 @@ use blog::Post;
 
 fn main() {
     let mut post = Post::new();
-
     post.add_text("I ate a salad for lunch today");
-    assert_eq!("", post.content());
 
-    post.request_review();
-    assert_eq!("", post.content());
+    let post = post.request_review();
 
-    post.reject();
-    assert_eq!("", post.content());
+    let mut post = post.reject();
+    post.add_text("[edited]");
 
-    post.request_review();
-    assert_eq!("", post.content());
-
-    post.approve();
-    assert_eq!("I ate a salad for lunch today", post.content());
+    let post = post.request_review();
+    let post = post.approve();
+    assert_eq!("I ate a salad for lunch today[edited]", post.content());
 }
