4afccea34a94186d2a51040d001111f265e19d36
NazarK0 <staticman999@gmail.com>
2022-06-20 14:42:07 +0300

    simple response with html

--- hellow/src/main.rs
+++ hellow/src/main.rs
@@ -1,5 +1,6 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::prelude::*;
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -12,10 +13,15 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP / 1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("hello.html").unwrap();
+
+    let response = format!(
+        "HTTP / 1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
     
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
