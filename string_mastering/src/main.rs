40f9b8d96ad651b0fcab2bb412ec4f39d4883e4b
NazarK0 <staticman999@gmail.com>
2022-06-26 20:03:35 +0300

    find duplicates

--- string_mastering/src/main.rs
+++ string_mastering/src/main.rs
@@ -1,70 +1,27 @@
-use std::mem;
 
 fn main() {
-    let mut s = String::from("Привіт💖 and hello!❤");
+    let mut s = String::from("абвггдеєжфазїҐ");
 
-    // delete_duplicates(&mut s);
-    let s_bytes = s.as_bytes();
+    let updated = delete_duplicates(&s);
 
-    println!("{:#?}, ",&s_bytes[0..4]);
-
-
-    print!("char sizes 1: ");
-    for ch in s.chars() {
-        print!("{:#?}, ", char_length(ch));
-    }
-    print!("\n");
+    println!("{}",updated);
 }
 
-fn char_length(ch: char) -> usize {
-    ch.to_string().as_bytes().iter().count()
-}
+fn delete_duplicates(source: &str) -> String {
+    let mut chars_vec = Vec::with_capacity(source.chars().count());
 
-fn find_duplicate_char_ids(src: &String) -> Vec<usize> {
-    let alphabet_length = src.chars().count();
-    let source_chars = src
-        .chars()
-        // skip last character
-        .take(alphabet_length - 1)
-        .enumerate();
-
-    let mut duplicate_indexes = Vec::<usize>::new();
-
-    for source_data in source_chars {
-        let source_idx = source_data.0;
-        let source_char = source_data.1;
-
-        let inspected_string = src.chars().skip(source_idx + 1).enumerate();
-
-        for insp_data in inspected_string {
-            let inspected_idx = insp_data.0;
-            let inspected_char = insp_data.1;
-
-            if source_char == inspected_char {
-                duplicate_indexes.push(inspected_idx + 1 + source_idx);
-            }
-        }
+    for ch in source.chars() {
+        chars_vec.push(ch);
     }
 
-    duplicate_indexes
-}
+    chars_vec.sort();
+    chars_vec.dedup();
 
-fn delete_duplicates(src: &mut String) {
-    let duplicate_indexes = find_duplicate_char_ids(src);
+    let mut result_string = String::new();
 
-    for data in duplicate_indexes.iter().enumerate() {
-        let idx = data.0;
-        let duplicate_idx = data.1;
-
-        // let substring_left = &src[0..duplicate_idx -1];
-        // let start = duplicate_idx;
-        // let end = duplicate_idx + 2;
-        // let substring_right = &src[start..end];
-
-        src.remove(duplicate_idx - idx);
+    for ch in chars_vec.iter() {
+        result_string.push(*ch);
     }
-}
 
-fn byte_idx(source: &str, char_idx: usize) -> usize {
-    
+    result_string
 }
